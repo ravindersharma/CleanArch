7b41db0d1a42a4cd3cece2d489e40b096d1febae
ravindersharma <ravindersharma583@gmail.com>
2024-10-17 18:09:05 +0530

    Added Course Domain model

---
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }    
+        public required string Description { get; set; }
+        public required string ImgUrl { get; set; }
+        
+    }
+}
