7bc399a60e8b7c3ed5cb57df9dc518b9447d9213
ravindersharma <ravindersharma583@gmail.com>
2024-10-17 18:59:10 +0530

    Commit implementation of Service and Repository

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+          return _context.Courses;
+        }
+    }
+}
