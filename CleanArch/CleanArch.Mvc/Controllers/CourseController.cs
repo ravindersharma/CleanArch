4a34b12582f434b4642c601d76a9b3d5aac9022a
ravindersharma <ravindersharma583@gmail.com>
2024-10-22 16:07:11 +0530

    Did controller implementation and add authrization

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService coruseService) { 
+           _courseService = coruseService;
+        }
+
+        public IActionResult Index()
+        {
+            var model=_courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
